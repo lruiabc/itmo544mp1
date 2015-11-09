32e70a78bb8a400571782da29d13465b65861819
lruiabc <568047669@qq.om>
2015-11-09 05:21:16 -0600

    added a new setup.php from build the table in the db

--- setup.php
+++ setup.php
@@ -2,35 +2,7 @@
 require 'vendor/autoload.php';
 use Aws\Rds\RdsClient;
 
-/*
-$link = mysqli_connect("itmo544grh-mp1.ckh0wwv6itjw.us-west-2.rds.amazonaws.com","rui","110224Fish","itmoruidb") or die("Error " . mysqli_error($link));
- check connection 
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-$sql = "CREATE TABLE comments 
-(
-    id INT NOT NULL AUTO_INCREMENT,
-    uname VARCHAR(20),
-    email VARCHAR(200) NOT NULL,
-    phone VARCHAR(20) NOT NULL,
-    filename VARCHAR(255) NOT NULL,
-    s3rawurl VARCHAR(255) NOT NULL,
-    s3finishedurl VARCHAR(255) NOT NULL,
-    jpg_filename VARCHAR(255),
-    status INT NOT NULL,
-    issubscribed INT NOT NULL,
-    timestamp dtaetime,
-    PRIMARY KEY(id)
-)";
-$con=$link->query($sql);
 
-$client = RdsClient::factory(array(
-'version' =>'latest',
-'region'  => 'us-west-2'
-));
-*/
 
 $client = RdsClient::factory(array(
 'version' =>'latest',
@@ -52,16 +24,7 @@ if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
-/*
-$delete_table = 'DELETE TABLE comments';
-$del_tbl = $link->query($delete_table);
-if ($delete_table) {
-        echo "Table student has been deleted";
-}
-else {
-        echo "error!!";
-}
-*/
+
 $create_table = 'CREATE TABLE IF NOT EXISTS items  
 (
     id INT NOT NULL AUTO_INCREMENT,
@@ -88,6 +51,7 @@ else {
 $link->close();
 
 ?>
-~                                                                                                                                                                                                            
-~                                                                                                                                                                                                            
-~       
+ <?php
+$output = shell_exec('chmod.sh');
+echo "<pre>$output</pre>";
+?>    
